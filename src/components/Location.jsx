4897f49049311ebe699d1a16db40b5d239cfb284
Adrian <adrianivsdk@gmail.com>
2020-03-03 02:50:57 -0300

    Sales completed with react-chartsjs-2

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import Map from "./Map";
+import "../styles/Style.scss";
 
 function Location(props) {
-  return <Map></Map>;
+  return (
+    <div className="location box-card">
+      <div className="container">
+        <h3 className="spacing card-title">Local</h3>
+        <div style={{ height: "90%", width: "100%" }}>
+          <Map></Map>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Location;
