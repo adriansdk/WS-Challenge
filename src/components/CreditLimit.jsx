fbbce302b0175f43ff90974112252193c56dd02b
Adrian <adrianivsdk@gmail.com>
2020-03-03 03:12:23 -0300

    Fixed some styling on other components and finalized credit-limit session

--- src/components/CreditLimit.jsx
+++ src/components/CreditLimit.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import "../styles/Style.scss";
 
 function CreditLimit(props) {
   return (
     <div className="credit-limit box-card">
-      <h3>LIMITE</h3>
+      <div className="container">
+        <h3 className="spacing card-title">Limite de crédito</h3>
+        <div className="row">
+          <h4 className="blue">
+            R$12,000,20 <br></br> <span>Concedido</span>
+          </h4>
+        </div>
+        <div className="row">
+          <h4 className="green">
+            R$3,105,00 <br></br> <span>Disponível</span>
+          </h4>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/FinancialTittle.jsx
+++ src/components/FinancialTittle.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function FinancialTittle(props) {
-  return <div className="financial-tittle box-card">assas</div>;
+  return (
+    <div className="activities box-card">
+      <div className="container">
+        <h3 className="spacing card-title">Títulos financeiros</h3>
+      </div>
+    </div>
+  );
 }
 
 export default FinancialTittle;
